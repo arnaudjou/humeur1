491eca5443b98727447b3e3eace80914cf848a0f
arnaudjou <arno.aij@gmail.com>
2018-12-06 12:19:57 +0100

    configure CommentaryButton and HistoryButton on MainActivity

--- app/src/main/java/com/example/nono/humeur/controller/controller/fragment/MainActivity.java
+++ app/src/main/java/com/example/nono/humeur/controller/controller/fragment/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.ImageButton;
 
 
 import com.example.nono.humeur.R;
 import com.example.nono.humeur.controller.adapter.PageAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton HistoryButton;
+    ImageButton CommentaryButton;
+
 
 
     @Override
@@ -20,7 +25,31 @@ public class MainActivity extends AppCompatActivity {
 
         this.configureViewPager ();
 
+        addListenerOnButton();
+
     }
+
+    private void addListenerOnButton() {
+
+        HistoryButton = (ImageButton) findViewById(R.id.HistoryButton);
+
+        HistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        CommentaryButton = (ImageButton) findViewById(R.id.CommentaryButton);
+
+        CommentaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
 
     private void configureViewPager (){
         ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
