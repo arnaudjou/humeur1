a74d43558b62dbf2519d51dfe131f870a4b116c7
arnaudjou <arno.aij@gmail.com>
2018-12-17 20:33:08 +0100

    configure shared preferences and test toast for happyButton

--- app/src/main/java/com/example/nono/humeur/controller/controller/fragment/fragments/View1Fragment.java
+++ app/src/main/java/com/example/nono/humeur/controller/controller/fragment/fragments/View1Fragment.java
@@ -2,28 +2,36 @@ package com.example.nono.humeur.controller.controller.fragment.fragments;
 
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.DeadSystemException;
+import android.sax.StartElementListener;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 import com.example.nono.humeur.R;
-import com.example.nono.humeur.controller.controller.fragment.MainActivity;
 
+import static java.time.DayOfWeek.MONDAY;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class View1Fragment extends Fragment {
 
-    private ImageButton buttonH;
-    private ImageButton historyButton;
+    public static int DAY_OF_WEEK;
+
 
     public View1Fragment() {
     }
@@ -62,6 +70,42 @@ public class View1Fragment extends Fragment {
                 dialog.show();
             }
         });
+
+        ImageButton view1;
+        view1 = view.findViewById(R.id.View1);
+        view1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences mHappy = getActivity().getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = mHappy.edit();
+                editor.putLong("day", DAY_OF_WEEK );
+                editor.commit();
+                Toast.makeText(getActivity(), "your mood is record",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageButton historyButton;
+        historyButton = view.findViewById(R.id.HistoryButton);
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(View1Fragment.this.getActivity());
+                View mView = getLayoutInflater().inflate(R.layout.activity_commentary, null);
+                mBuilder.setView(mView).setNegativeButton("Later", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+            }
+        });
+
         return view;
     }
 }
